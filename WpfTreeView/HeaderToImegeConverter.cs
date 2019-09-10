3175365658d426a721da1c95c89041fd16f7cdd6
Тарас <@DESKTOP-PGEAAM8>
2019-09-10 12:03:06 +0300

    Implemented class HeaderToImageConverter

--- WpfTreeView/HeaderToImegeConverter.cs
+++ WpfTreeView/HeaderToImegeConverter.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Globalization;
-
+using System.IO;
 using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace WpfTreeView
 {
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
     class HeaderToImegeConverter : IValueConverter
     {
+        public static HeaderToImegeConverter Instance = new HeaderToImegeConverter();
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            var path = (string)value;
+            if(path == null)
+            {
+                return null;
+            }
+            var name = MainWindow.GetileFolderName(path);
+            var image = "Images/file.png";
+            if (string.IsNullOrEmpty(name))
+            {
+                image = "Images/drive.png";
+            }
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                image = "Images/folder-closed.png";
+            }
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)

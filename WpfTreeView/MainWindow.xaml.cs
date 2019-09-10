99d72aca188c6fecd5c1d16659aad948892f5850
Тарас <@DESKTOP-PGEAAM8>
2019-09-10 11:09:15 +0300

    First commit TreeView

--- WpfTreeView/MainWindow.xaml.cs
+++ WpfTreeView/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfTreeView
 {
@@ -23,6 +15,88 @@ namespace WpfTreeView
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                item.Header = drive;
+                item.Tag = drive;
+
+                item.Items.Add(null);
+
+                item.Expanded += Folder_Expended;
+
+                FolderView.Items.Add(item);
+            }
+
+        }
+
+        private void Folder_Expended(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            if (item.Items.Count != 1 || item.Items[0] != null)
+                return;
+            item.Items.Clear();
+            var fullPath = (string)item.Tag;
+            var directories = new List<string>();
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+            }
+            catch { }
+
+            directories.ForEach(directoryPath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetileFolderName(directoryPath),
+                    Tag = directoryPath
+                };
+                subItem.Items.Add(null);
+                subItem.Expanded += Folder_Expended;
+                item.Items.Add(subItem);
+
+            });
+
+            var files = new List<string>();
+            try
+            {
+                var fs = Directory.GetFiles(fullPath);
+                if (fs.Length > 0)
+                    files.AddRange(fs);
+            }
+            catch { }
+
+            files.ForEach(filePath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = GetileFolderName(filePath),
+                    Tag = filePath
+                };
+      
+                item.Items.Add(subItem);
+
+            });
+
+        }
+        public static string GetileFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+            var normalizedPath = path.Replace('/', '\\');
+            var lastIndex = normalizedPath.LastIndexOf('\\');
+            if (lastIndex <= 0)
+                return path;
+
+            return path.Substring(lastIndex + 1);
+                     
         }
     }
 }
